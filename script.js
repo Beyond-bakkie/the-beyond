856415d9e1077cbb824317649377e649376141a5
Beyond-bakkie <beyond.repairs@outlook.com>
2025-08-09 14:46:49 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const splash = document.getElementById("splash");
+  const firstCarousel = document.getElementById("first-carousel");
+
+  // Wait for splash fade to finish
+  setTimeout(() => {
+    splash.style.display = "none"; // remove it completely
+    firstCarousel.scrollIntoView({ behavior: "smooth" });
+  }, 2500); // matches fade animation timing
+});
